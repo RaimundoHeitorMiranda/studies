40b1b2c821f037dc32a4d32959830ea26e1eca2a
Raimundo <raimundo.heitor.sm@gmail.com>
2022-01-12 22:18:30 -0300

    Capitulo inical e parte do código de exemplo

---
+++ Refatoração-Martin-Fowler/CAP1_primeiro_exemplo/main.js
@@ -0,0 +1,33 @@
+function statement (invoice, plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = new Intl.NumberFormat("en-US", {style: "currency", currency:"USD", minimumFractionDigits:2}).format;
+
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID];
+        let thisAmount = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000
+                if (perf.audience > 30){
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000
+                if (perf.audience > 20){
+                    thisAmount += 1000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience
+                break
+            default:
+                throw new Error(`unknown type: ${play.type}`)
+            
+        }
+        // Soma créditos por volume
+        volumeCredits += Math.max(perf.audience -30, 0)
+        // Soma um crédito extra para cada dez espectadores de comédia
+    }
+}
